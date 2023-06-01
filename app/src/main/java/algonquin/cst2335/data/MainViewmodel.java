1efd7ac302c0619b66f5a64ef8dd04b832c4da25
Loveleen <lovel0115@algonquinlive.com>
2023-06-01 01:16:28 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/data/MainViewmodel.java
+++ app/src/main/java/algonquin/cst2335/data/MainViewmodel.java
@@ -1,11 +1,9 @@
 package algonquin.cst2335.data;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 public class MainViewmodel extends ViewModel {
     public String theText;
-    public String buttonText;
-    public MutableLiveData<String> editString =new MutableLiveData();
+   public String buttonText;
+   public MutableLiveData<String> editString =new MutableLiveData();
     public MutableLiveData<Boolean> isSelected=new MutableLiveData<>();
 }
--- app/src/main/java/algonquin/cst2335/love0115/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/love0115/ui/MainActivity.java
@@ -1,119 +1,51 @@
 package algonquin.cst2335.love0115.ui;
 
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.Switch;
-import android.widget.Toast;
-import android.widget.TextView;
 
 import algonquin.cst2335.data.MainViewmodel;
 import algonquin.cst2335.love0115.R;
 import algonquin.cst2335.love0115.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
     private MainViewmodel model;
     private ActivityMainBinding variableBinding;
     private ViewModelProvider viewModelProvider;
-
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        model = new ViewModelProvider(this).get(MainViewmodel.class);
+        setContentView(R.layout.activity_main);
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.spin_switch);
 
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());//loads XML on screen
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.spin_switch);
 
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-        TextView theText=variableBinding.textview;
-        Button myButton= variableBinding.myButton;
-        ImageButton myImageButton = variableBinding.myimagebutton;
-
-
-        model.editString.observe(this,s -> {
-            variableBinding.textview.setText("Your edit text has "+s);
-
-        });
-
-
-        variableBinding.myButton.setOnClickListener(click -> {
-            model.editString.postValue(variableBinding.myedittext.getText().toString());
-            // model.editString = variableBinding.myedittext.getText().toString();
-            // variableBinding.textview.setText("Your edit has:" + model.editString);
-        });
-
-             //show a toast message
-
-        //calling setOnCheckedChangeListener method on each button
-        CheckBox mycheckbox=variableBinding.mycheckbox;
-        Switch mySwitch=variableBinding.mySwitch;
-        RadioButton myRadiobutton=variableBinding.myRadiobutton;
-        ImageView myImageView=variableBinding.myImageView;
-
-        mycheckbox.setOnCheckedChangeListener((mycheckboxox,isChecked)->{
-            //b is a new value on/off
-            model.isSelected.postValue(isChecked);
-        });
-        mySwitch.setOnCheckedChangeListener((s,isChecked)->{
-            model.isSelected.postValue(isChecked);
-        });
-        myRadiobutton.setOnCheckedChangeListener((radio,isChecked)->{
-            model.isSelected.postValue(isChecked);
-        });
-        /*mycheckbox.setOnCheckedChangeListener((a,b)->{
-            //b is a new value on/off
-            theText.setText("Do you drink coffee?"+b);
-        });
-
-         mySwitch.setOnCheckedChangeListener((a,b)->{
-             theText.setText("Do you drink coffe?"+b);
-        });
-
-         myRadiobutton.setOnCheckedChangeListener((a,b)->{
-             theText.setText("Do you drink coffee?"+b);
-         });
-
-         */
-        model.isSelected.observe(this,selected->{
-            mycheckbox.setChecked(selected);
-            mySwitch.setChecked(selected);
-            myRadiobutton.setChecked(selected);
-            // Show a toast message
-            String toastMessage = "The value is now: " + selected;
-            Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-
-        });
-
-        //ser image fresource for image view
-        myImageView.setImageResource(R.drawable.algonquin);
-        model.editString.observe(this,s -> {
-            variableBinding.textview.setText("Your edit text has"+s);
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
 
 
-        });
-
-
-        ///variableBinding.textview.setText(model.editString);
-        variableBinding.myButton.setOnClickListener(click -> {
-            model.editString.postValue(variableBinding.myedittext.getText().toString());
-            // model.editString = variableBinding.myedittext.getText().toString();
-            // variableBinding.textview.setText("Your edit has:" + model.editString);
-        });
-        //ImageButton myImageButton = variableBinding.myimagebutton;
-        myImageButton.setOnClickListener(click -> {
-            int width = myImageButton.getWidth();
-            int height = myImageButton.getHeight();
-            String toastMessage = "The width = " + width + " and height = " + height;
-            Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-        });
-
+    });
 
 
     }
@@ -125,60 +57,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-///part5
-//theText.setText(model.editString);
-        /*CheckBox mycheckbox=variableBinding.mycheckbox;
-        Switch mySwitch=variableBinding.mySwitch;
-
-        mycheckbox.setOnCheckedChangeListener((a,b)->{
-            //b is a new value on/off
-            theText.setText("Do you drink coffee?"+b);
-        });
-
-       mySwitch.setOnCheckedChangeListener((a,b)->{theText.setText("Do you drink coffe?"+b);
-       });
-
-         */
-
-
-
-
-
-///part2
-
-        /*setContentView(R.layout.activity_main);
-
-        TextView mytext = findViewById(R.id.textview);//this will lok for something with id in text and it does return something or null
-        //TextView mytext=variableBinding.textview;
-
-        Button btn = findViewById(R.id.button_id);//it should matches the button tag in .xml
-
-        EditText myedit = findViewById(R.id.myedittext);
-        String editString = myedit.getText().toString();
-
-        btn.setText("something new here");
-
-
-
-
-            //onclick function with v parameter , body
-       btn.setOnClickListener((vw) -> {
-
-            mytext.setText( "Your edit text is "+editString );
-            btn.setText("something new here");
-        });
-
-
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-               String editString = myedit.getText().toString();
-                mytext.setText( "Your edit text has "+editString );
-                btn.setText("something new here");
-
-         */
 
